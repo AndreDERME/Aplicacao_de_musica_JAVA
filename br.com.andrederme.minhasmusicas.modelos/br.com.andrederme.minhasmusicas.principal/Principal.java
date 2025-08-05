d5018d002eb3ef0b1f3a4741665a82476c18f736
Andre Derme <andredermee@gmail.com>
2025-08-05 10:35:19 -0300

    Create Principal.java

---
+++ br.com.andrederme.minhasmusicas.modelos/br.com.andrederme.minhasmusicas.principal/Principal.java
@@ -0,0 +1,26 @@
+package br.com.minhasmusicas.principa;
+
+public class Principal{
+    public static void main(String[] args){
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("forever");
+        minhaMusica.setCantor ("kiss");
+        
+        
+        for (int i = 0; i < 1000; i++){
+            minhaMusica.reproduz();
+        }
+
+        podcast meuPodcast = new podcast();
+        meuPodcast.setTitulo("Bolhadev");
+        meuPodcast.setApresentador("Marcos mendes")
+    }
+
+    for (int i = 0; i < 5000; i++){
+        meuPodcast.reproduz;
+    }
+
+    for (int i = 0; i < 5000; i++){
+        meuPodcast.curte();
+    }
+}
