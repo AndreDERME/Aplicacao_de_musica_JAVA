b9dfefafc43ea0c260bdb453379431b8ae0d2453
Andre Derme <andredermee@gmail.com>
2025-08-05 10:35:15 -0300

    Update podcast.java

--- br.com.andrederme.minhasmusicas.modelos/podcast.java
+++ br.com.andrederme.minhasmusicas.modelos/podcast.java
@@ -1,1 +1,23 @@
 package br.com.andrederme.minhasmusicas.modelos
+
+ public class podcast extends Audio{
+    private String descricao;
+    private String apresentador;
+
+    public String getDescricao(){
+        return descricao
+    }
+
+    public void setDescricao (String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getApresentador(){
+        return apresentador;
+    }
+
+    public void setApresentador (String apresentador){
+        this.apresentador = apresentador;
+    }
+
+ }
